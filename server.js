a7b09d0798179e62408e380c2a8e9b6fba830366
Gabriel Diniz Cremel <dinizcremelgabriel@gmail.com>
2024-09-24 17:36:35 -0300

    set up Express server with routes and 404 handling

--- server.js
+++ server.js
@@ -1,40 +1,24 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
-// Set the view engine to Pug
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-// Set the views directory
-app.set("views", path.join(__dirname, "views"));
-
-// Set the basedir option for Pug to allow using absolute paths
-app.locals.basedir = path.join(__dirname, "public");
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
-
-// Import adminData (both routes and products)
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-// Use '/admin' as the base path for admin-related routes
-app.use("/admin", adminData.routes);
-
-// Use '/' as the base path for shop-related routes
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).render("page-not-found", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
-// Start the server
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(3000);
