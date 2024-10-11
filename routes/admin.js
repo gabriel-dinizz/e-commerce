712cab7b701a9c37037e0ff2c3c1a12ecdcc8a3b
Gabriel Diniz <gabrieldiniz@MacBook-Air-de-Gabriel.local>
2024-10-10 21:41:26 -0300

    commit

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,25 @@
-const path = require("path");
-
-const express = require("express");
-
-const productsController = require("../controllers/products");
-
+const express = require('express');
 const router = express.Router();
 
+const products = []; // This array will store products
+
 // /admin/add-product => GET
-router.get("/add-product", productsController.getAddProduct);
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+});
 
 // /admin/add-product => POST
-router.post("/add-product", productsController.postAddProduct);
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
 
-module.exports = router;
+// Exporting both the router and products array
+exports.router = router;
+exports.products = products;
